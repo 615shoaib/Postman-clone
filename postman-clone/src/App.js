d2f8674517c8bf09a0660678ac50faf50ed80268
Shoaib <615shoaib@gmail.com>
2023-04-17 15:48:31 +0500

    postman-clone

--- postman-clone/src/App.js
+++ postman-clone/src/App.js
@@ -1,10 +1,11 @@
 
-import {Button} from "@mui/material"
+import {Button, Typography} from "@mui/material"
 function App() {
   return (
   <>
     <Button>Click</Button>
     <h1>Helo github</h1>
+    <Typography
   </>
    
   );
